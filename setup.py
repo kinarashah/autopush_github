de19124185cf73ce84fc5bd5eb6e50cf2682f940
Kinara Shah <kinarahs@usc.edu>
2017-04-22 16:44:39 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='autopush_github',
       version='0.1',
-      description='Automatically push solutions to Github',
+      description='Automatically push Leetcode solutions to Github',
       url='http://github.com/kinarashah/autopush-github',
       author='Kinara Shah',
       author_email='kinarahs@usc.edu',
